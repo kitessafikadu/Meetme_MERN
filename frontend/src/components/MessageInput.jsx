df84d1ac9cbbeb65ab96c90cc17660ceb9b6cc42
Kitessa Fikadu <kitessafikadu@gmail.com>
2025-01-25 19:07:55 +0300

    Implement image preview and removal functionality in MessageInput component

--- frontend/src/components/MessageInput.jsx
+++ frontend/src/components/MessageInput.jsx
@@ -1,5 +1,42 @@
+import { useRef, useState } from "react";
+import { useChatStore } from "../store/useChatStore";
+import { Image, Send, X } from "lucide-react";
+import toast from "react-hot-toast";
+
 function MessageInput() {
-  return <div>MessageInput</div>;
+  const [text, setText] = useState("");
+  const [imagePreview, setImagePreview] = useState(null);
+  const fileInputRef = useRef(null);
+  const { sendMessage } = useChatStore();
+
+  const handleImageChange = (e) => {};
+
+  const removeImage = () => {};
+
+  const handleSendMessage = async (e) => {};
+  return (
+    <div className="p-4 w-full">
+      {imagePreview && (
+        <div className="mb-3 flex items-center gap-2">
+          <div className="relative">
+            <img
+              src={imagePreview}
+              alt="Preview"
+              className="w-20 h-20 object-cover rounded-lg border border-zinc-700"
+            />
+            <button
+              onClick={removeImage}
+              className="absolute -top-1.5 -right-1.5 w-5 h-5 rounded-full bg-base-300
+              flex items-center justify-center"
+              type="button"
+            >
+              <X className="size-3" />
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default MessageInput;
