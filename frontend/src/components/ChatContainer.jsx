8ce0cd8b78e7044041e565e28d59bed387694fe6
Kitessa Fikadu <kitessafikadu@gmail.com>
2025-01-28 09:55:26 +0300

    Comment out message subscription logic in ChatContainer and useChatStore components

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -19,18 +19,18 @@ const ChatContainer = () => {
   const { authUser } = useAuthStore();
   const messageEndRef = useRef(null);
 
-  useEffect(() => {
-    getMessages(selectedUser._id);
+  // useEffect(() => {
+  //   getMessages(selectedUser._id);
 
-    subscribeToMessages();
+  //   subscribeToMessages();
 
-    return () => unsubscribeFromMessages();
-  }, [
-    selectedUser._id,
-    getMessages,
-    subscribeToMessages,
-    unsubscribeFromMessages,
-  ]);
+  //   return () => unsubscribeFromMessages();
+  // }, [
+  //   selectedUser._id,
+  //   getMessages,
+  //   subscribeToMessages,
+  //   unsubscribeFromMessages,
+  // ]);
 
   useEffect(() => {
     if (messageEndRef.current && messages) {
